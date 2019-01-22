fc003ab1147a1c823e4f7e732b2211a77a590ab9
albertalbertalbert <46792463+albertalbertalbert@users.noreply.github.com>
2019-01-22 16:59:06 -0500

    Add files via upload

--- tak.lisp
+++ tak.lisp
@@ -1,25 +1,39 @@
-
+;;;; Variable definitions
 (defparameter *board*
   (list '(W W) '(Y W) nil nil '(B B B)
         '(W W) '(W W) '(W W B) nil nil
         nil '(W) '(W)  '(W B W)  nil
         nil nil nil '(W)  '(W W) 
-        nil nil nil nil nil ))
+        nil nil nil nil nil ));;sample for testing
 (defvar *side* '(WHITE BLACK))
 (defvar *pieces* '(road blocker capstone))
+(defparameter *piece-map* (make-hash-table :test 'equal))
+(setf (gethash '(road white) *piece-map*) 'W)
+(setf (gethash '(road black) *piece-map*) 'B)
+(setf (gethash '(blocker white) *piece-map*) 'X)
+(setf (gethash '(blocker black) *piece-map*) 'C)
+(setf (gethash '(capstone white) *piece-map*) 'Y)
+(setf (gethash '(capstone black) *piece-map*) 'Z)
+
+
 
 (defvar *last-col* (list 4 9 14 19 24))
-(defun initialize-board ()
-  (list nil nil nil nil nil
-        nil nil nil nil nil
-        nil nil nil nil nil
-        nil nil nil nil nil
-        nil nil nil nil nil))
+(defvar *start* (list '(0) '(5) ))
+
 (defvar *directions* (list 'up 'down 'left 'right))
 
 (defvar *header* "----------------------------------------------")
 (defvar *separator* "----------------------------------------------")
 (defvar *footer* "----------------------------------------------")
+;;;; Variable definition complete
+
+(defun initialize-tak-board ()
+  (list nil nil nil nil nil
+        nil nil nil nil nil
+        nil nil nil nil nil
+        nil nil nil nil nil
+        nil nil nil nil nil))
+
 (defun print-board (board)
   (format t "~%~a~%" *header*)
   (dotimes (i 5)
@@ -30,31 +44,21 @@
 (defun evaluate (moves)
   (first moves))
 
-
-
 (defun adjacent (cell direction)
   (if (or (< cell 0) (> cell 24)) (error "Invalid cell!"))
-  (case direction
+  (ecase direction
     (up (and (> cell 4) (- cell 5)))
     (down (and (< cell 20) (+ cell 5)))
     ((right) (and (/= 4 (mod cell 5)) (+ 1 cell)))
     ((left) (and (/= 0 (mod cell 5)) (- cell 1)))))
 
-
 (defun next-cell (cell board path)
   (let ((next (remove-if #'null (mapcar (lambda (x) (adjacent cell x) ) *directions*) )))
     (setf next (remove-if #'null (remove-if-not (lambda (x) (eq (first (nth x board)) (first (nth cell board)))) next)))
-    ;(setf next (remove-if (lambda (x) (member x path)) next))
     (mapcar (lambda (x) (if (member x path)
                             path
                           (cons x path))) next)))
 
-#|
-(defun new-paths (board paths)
-  (remove-duplicates
-   (reduce #'append (mapcar (lambda (x) (next-cell (first x) board x)) paths))
-   :test #'equal))
-|#
 (defun new-paths (board paths)
   (remove-duplicates
    (mapcan (lambda (x) (next-cell (first x) board x)) paths)
@@ -68,14 +72,15 @@
       (fixed-point arg1 result))))
 
 (defun winning (board)
-  (reduce #'append (mapcar (lambda (y)
-                             (remove-if-not 
-                              (lambda (x) (member y x) )
-                              (fixed-point board *start*)))
-                           *last-col*)))
-  
+  (mapcan (lambda (y)
+            (remove-if-not 
+             (lambda (x) (member y x) )
+             (fixed-point board *start*)))
+          *last-col*))
 
 (defun moves (cell board side)
+  (when (null (member side *side*))
+    (error "Invalid side"))
   (cond
    ((null (nth cell board))
     (mapcar (lambda (x) (add-piece cell x board side)) *pieces*))))
@@ -85,7 +90,7 @@
   (let ((before-cell nil)
         (after-cell nil)
         (active-cell (nth cell board))
-        (new-piece nil))
+        (new-piece nil) )
     (cond
      ((= 0 cell) 
       (setf after-cell (rest board)))
@@ -94,24 +99,22 @@
      (t 
       (setf before-cell (subseq board 0 cell))
       (setf after-cell (subseq board (+ cell 1) 25))))
-      
-    (cond
-     ((eq piece 'road)
-      (if (eq side 'white)
-          (setf new-piece 'W)
-        (setf new-piece 'B)))
-     ((eq piece 'blocker)
-      (if (eq side 'white)
-          (setf new-piece 'x)
-        (setf new-piece 'C)))
-     ((eq piece 'capstone)
-      (if (eq side 'white)
-          (setf new-piece 'Y)
-        (setf new-piece 'Z))))
+
+    (setf new-piece (gethash (list piece side) *piece-map*))
+    (when (null new-piece)
+      (error "Could not determine type of new piece"))
 
     (concatenate 'list before-cell (list (push new-piece active-cell)) after-cell)))
 
-    
+(defun move-stack (cell board stack  dir)
+  (let ((stack-color 'W)
+        (new-cell (adjacent cell dir))
+        (if (or (eq stack-color ))))
+  
+  
+  
+    )
+
 
 
 
